4692a482ba9680eba1b3eb12798eac2f1bf69c2a
zarab <musfiqzarab@iut-dhaka.edu>
2019-02-07 23:39:55 +0600

    creating Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
   return (
     <div>
-      <div>App</div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
